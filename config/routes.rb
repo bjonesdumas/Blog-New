5914215844a2cc425d83ff99d2f8f92d01cd0b8c
bjonesdumas <bjonesdumas@gmail.com>
2015-12-22 04:25:27 -0500

    Created Partials For Nav & Footer

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-
+  root 'pages#home'
   get '/home', to: 'pages#home'
   resources :articles do
     resources :comments
   end
  
-  root 'welcome#index'
+
 
  
 end
